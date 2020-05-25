a5f99025ac56adad9f6181d45fd2b43c7eaeb59d
duzongyue <407106821@qq.com>
2020-05-25 12:03:20 +0800

    完成列表页面select的获取和显示

--- src/views/List.vue
+++ src/views/List.vue
@@ -19,23 +19,22 @@
     <div class="table-list">
       <div class="function-button">
         <el-button type="primary" @click="addTableData">新增</el-button>
+        <el-button type="primary" @click="editTableData">编辑</el-button>
+        <el-button type="primary" @click="delTableData">删除</el-button>
       </div>
-      <el-table
-        :data="tableData"
-        border
-        style="min-width: 100%">
+      <el-table :data="tableData" border
+                style="min-width: 100%"
+                @selection-change="checkboxData">
         <template v-for="header in tableAttr.columns">
           <template v-if="header.checkbox">
             <el-table-column :key="header.primaryKey"
-                             type="selection"
-            >
+                             type="selection">
             </el-table-column>
           </template>
           <template v-else>
             <el-table-column :key="header.primaryKey"
                              :prop="header.field"
-                             :label="header.title"
-            >
+                             :label="header.title">
             </el-table-column>
           </template>
         </template>
@@ -57,8 +56,13 @@
     <el-dialog title="编辑" :visible="editVisible" @close="closeEditPage">
       <el-form label-width="80px" ref="editFormRef" :model="editForm" :rules="editFormRules">
         <el-form-item v-for="parameter in editAttr" :label="parameter.title" :key="parameter.index"
-                      :prop="parameter.field" :rules="parameter.rules">
-          <el-input v-model="editForm[parameter.field]"></el-input>
+                      :prop="parameter.field" :rules="parameter.rules" v-show="!parameter.hide">
+          <template v-if="parameter.type == 'input'">
+            <el-input v-model="editForm[parameter.field]"></el-input>
+          </template>
+          <template v-else-if="parameter.type == 'tree'">
+            <el-input v-model="editForm[parameter.field]"></el-input>
+          </template>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -80,11 +84,13 @@
         search: [], // 搜索内容
         total: 0, // 数据总数
         currentPage: 1, // 当前页码
-        pageSize: 20, // 每页显示数量
+        pageSize: 10, // 每页显示数量
         editVisible: false, // 是否显示编辑框
         editAttr: [], // 编辑框参数  目前设计的为每次点击都需要重新加载
         editForm: {},
-        editFormRules: {}
+        editFormRules: {},
+        ids: [], // 页面选择的数据id
+        selects: {} // 数据字典保存位置
       }
     },
     created () {
@@ -100,6 +106,7 @@
         const { data: res } = await this.$http.get(this.baseUrl + '/getPageAttr')
         if (res.code === 200) {
           this.tableAttr = res.data
+          await this.initSelect(this.tableAttr.columns)
         } else {
           this.$message.error(res.msg)
         }
@@ -116,8 +123,50 @@
         if (res.code === 200) {
           this.total = res.data.total
           this.tableData = res.data.rows
+          this.dataTransform() // 将select的内容转换
         } else {
           this.$message.error(res.msg)
+        }
+      },
+      /* 将数据字典的值进行转换 */
+      dataTransform () {
+        for (const tableAttr of this.tableAttr.columns) {
+          const type = tableAttr.type
+          switch (type) {
+            case 'tree':
+              for (const tableData of this.tableData) {
+                tableData[tableAttr.field] = this.getTransformValue(tableAttr.field, tableData[tableAttr.field])
+              }
+          }
+        }
+      },
+      /* 获取数据字典对应的值 */
+      getTransformValue (field, value) {
+        const selects = this.selects[field]
+        for (const select of selects) {
+          if (select.id === value) {
+            return select.name
+          }
+        }
+        return null
+      },
+      /* 初始化数据字典 */
+      initSelect (tableAttrs) {
+        for (const tableAttr of tableAttrs) {
+          const type = tableAttr.type
+          switch (type) {
+            case 'tree':
+              this.getSelect(tableAttr)
+          }
+        }
+      },
+      /* 获取数字字典 */
+      async getSelect (table) {
+        if (Object.prototype.hasOwnProperty.call(table, 'url') && table.url !== null && table.url.length !== 0) {
+          const { data: res } = await this.$http.get(table.url)
+          if (res.code === 200) {
+            this.selects[table.field] = res.data
+          }
         }
       },
       /* 初始化搜索栏 */
@@ -151,9 +200,30 @@
         this.getTableData()
       },
       /* 打开编辑框 */
-      async addTableData (primaryKey) {
+      async addTableData () {
         this.editVisible = true
-        await this.getEditAttr(primaryKey)
+        await this.getEditAttr()
+      },
+      checkboxData (selectData) {
+        this.ids = []
+        selectData.forEach(data => {
+          this.ids.push(data.primaryKey)
+        })
+      },
+      async editTableData () {
+        this.editVisible = true
+        if (this.ids.length !== 1) {
+          this.editVisible = false
+          this.$message.error('未选择数据')
+          return
+        }
+        await this.getEditAttr(this.ids[0])
+      },
+      /* 删除数据 */
+      async delTableData () {
+        await this.$http.delete(this.baseUrl + '/del', { params: { ids: this.ids.join(',') } })
+        await this.getTableData()
+        console.log(this.ids)
       },
       /* 关闭编辑框 */
       closeEditPage () {
@@ -165,17 +235,19 @@
       /* 获取编辑页面参数 */
       // TODO 需要重新整理from菜单 当前的办法太笨啦
       async getEditAttr (primaryKey) {
-        const { data: res } = await this.$http.get(this.baseUrl + '/getEditAttr')
+        const { data: res } = await this.$http.get(this.baseUrl + '/getEditAttr', { params: { primaryKey: primaryKey } })
         if (res.code === 200) {
           this.editAttr = []
           this.editForm = {}
           res.data.columns.forEach(attr => {
             const rules = this.buildUpRules(attr.rules, attr.title)
-            this.$set(this.editForm, attr.field, '')
+            this.$set(this.editForm, attr.field, attr.value)
             const formItem = {
               title: attr.title,
               field: attr.field,
-              rules: rules
+              rules: rules,
+              hide: attr.hide,
+              type: attr.type
             }
             this.editAttr.push(formItem)
           })
